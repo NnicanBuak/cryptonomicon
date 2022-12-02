22df1e852ffbfe57e0a8da91bd8da3dda62ac4fd
NnicanBuak <mybailengdb@gmail.com>
2022-12-02 18:29:14 +0800

    add new features: filter & pagination

--- src/App.vue
+++ src/App.vue
@@ -114,11 +114,119 @@
           Добавить
         </button>
       </section>
+
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 bg-gray-100 pb-8" />
+        <div :class="{ 'pb-8': this.tickers.length <= 6 }" class="bg-gray-100">
+          <div class="filter flex content-center">
+            <div
+              style="border-radius: 0.375rem 0 0 0.375rem"
+              class="relative p-2 rounded-md shadow-md bg-gray-200"
+            >
+              <svg
+                class="w-4 h-4"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 487.95 487.95"
+                style="enable-background: new 0 0 487.95 487.95"
+                xml:space="preserve"
+              >
+                <g>
+                  <g>
+                    <path
+                      d="M481.8,453l-140-140.1c27.6-33.1,44.2-75.4,44.2-121.6C386,85.9,299.5,0.2,193.1,0.2S0,86,0,191.4s86.5,191.1,192.9,191.1,c45.2,0,86.8-15.5,119.8-41.4l140.5,140.5c8.2,8.2,20.4,8.2,28.6,0C490,473.4,490,461.2,481.8,453z M41,191.4,c0-82.8,68.2-150.1,151.9-150.1s151.9,67.3,151.9,150.1s-68.2,150.1-151.9,150.1S41,274.1,41,191.4z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <input
+              v-model="tickersFilter"
+              style="border-radius: 0 0.375rem 0.375rem 0"
+              class="h-8 border-l-2 border-gray-300 rounded-md shadow-md bg-gray-200"
+              type="text"
+            />
+          </div>
+
+          <div v-if="this.tickers.length > 6" class="pagination flex p-2 gap-2">
+            <button
+              :disabled="this.tickersPage <= 1"
+              @click="this.tickersPage = this.tickersPage - 1"
+              class="border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              <svg
+                class="w-6 h-6"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 384.97 384.97"
+                style="enable-background: new 0 0 384.97 384.97"
+                xml:space="preserve"
+              >
+                <g>
+                  <g id="Chevron_Left_Circle">
+                    <path
+                      d="M192.485,0C86.185,0,0,86.185,0,192.485C0,298.797,86.173,384.97,192.485,384.97S384.97,298.797,384.97,192.485
+			C384.97,86.185,298.797,0,192.485,0z M192.485,361.282c-92.874,0-168.424-75.923-168.424-168.797S99.611,24.061,192.485,24.061
+			s168.424,75.55,168.424,168.424S285.359,361.282,192.485,361.282z"
+                    />
+                    <path
+                      d="M235.878,99.876c-4.704-4.74-12.319-4.74-17.011,0l-83.009,84.2c-4.572,4.62-4.584,12.56,0,17.191l82.997,84.2
+			c4.704,4.74,12.319,4.74,17.011,0c4.704-4.752,4.704-12.439,0-17.191l-74.528-75.61l74.54-75.61
+			C240.57,112.315,240.57,104.628,235.878,99.876z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </button>
+            <span class="text-xl">{{ this.tickersPage }}</span>
+            <button
+              :disabled="!tickersHasNextPage"
+              @click="this.tickersPage = this.tickersPage + 1"
+              class="border border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              <svg
+                class="w-6 h-6"
+                version="1.1"
+                id="Ebene_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="64px"
+                height="64px"
+                viewBox="0 0 64 64"
+                enable-background="new 0 0 64 64"
+                xml:space="preserve"
+              >
+                <g>
+                  <path
+                    d="M28.373,13.546c-0.803-0.758-2.068-0.722-2.827,0.081c-0.758,0.803-0.722,2.069,0.081,2.827L42.087,32l-16.46,15.546
+		c-0.803,0.758-0.839,2.024-0.081,2.827C25.939,50.79,26.469,51,27,51c0.493,0,0.986-0.181,1.373-0.546l18-17
+		C46.773,33.076,47,32.55,47,32s-0.227-1.076-0.627-1.454L28.373,13.546z"
+                  />
+                  <path
+                    d="M32,0C23.453,0,15.417,3.329,9.374,9.373C3.329,15.417,0,23.453,0,32s3.33,16.583,9.374,22.626
+		C15.417,60.671,23.453,64,32,64s16.583-3.329,22.626-9.373C60.671,48.583,64,40.547,64,32s-3.33-16.583-9.374-22.626
+		C48.583,3.329,40.547,0,32,0z M51.797,51.798C46.509,57.087,39.479,60,32,60s-14.509-2.913-19.798-8.202C6.913,46.51,4,39.479,4,32
+		s2.913-14.51,8.203-19.798C17.491,6.913,24.521,4,32,4s14.509,2.913,19.798,8.202C57.087,17.49,60,24.521,60,32
+		S57.087,46.51,51.797,51.798z"
+                  />
+                </g>
+              </svg>
+            </button>
+          </div>
+        </div>
         <dl class="pb-8 grid grid-cols-1 gap-5 bg-gray-100 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="selectTicker(t)"
             :class="{
@@ -158,6 +266,7 @@
         </dl>
         <hr class="w-full border-t border-gray-600 pt-8" />
       </template>
+
       <section v-show="selectedTickerName !== null" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 mt-8">
           {{ this.selectedTickerName }} - USD
@@ -165,13 +274,14 @@
         <h3 class="bold text-lg leading-3 font-light text-gray-900 mb-8">
           {{ this.graph[0]?.value }}
         </h3>
-        <div
+        <di
           @mousewheel="scaleGraph"
+          :alt="this.graphScale"
           :class="{ 'graph-scaled': this.graphIsScaling }"
           class="overflow-hidden flex flex-row-reverse border-gray-600 border-b border-l h-64"
         >
           <div
-            v-for="(bar, index) in normalizeGraph(this.graph)"
+            v-for="(bar, index) in normalizedGraph()"
             :key="index"
             class="h-full flex items-end"
           >
@@ -184,7 +294,7 @@
               class="bg-purple-800 border w-16"
             ></div>
           </div>
-        </div>
+        </di>
         <button
           @click="selectTicker()"
           type="button"
@@ -238,6 +348,9 @@ export default {
         { name: "BCH", show: true },
       ],
       tickers: [], // { name: "", value: [] }
+      tickersFilter: "",
+      tickersPage: 1,
+      tickersHasNextPage: false,
       selectedTickerName: null, // ""
       graph: [],
       graphScale: 2.25,
@@ -246,13 +359,12 @@ export default {
   },
 
   created() {
-    (async () => {
-      const promise = await fetch(
-        "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
-      );
-      const data = await promise.json();
-      this.coinList = Object.keys(data.Data);
-    })();
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) this.tickersFilter = windowData.filter;
+    if (windowData.page) this.tickersPage = windowData.page;
 
     const tickersData = localStorage.getItem("cryptonomicon-list");
     if (tickersData) {
@@ -263,6 +375,13 @@ export default {
     this.searchHintsDefault
       .filter((h) => this.tickers.some((t) => t.name === h.name))
       .forEach((h) => (h.show = false));
+    (async () => {
+      const promise = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
+      );
+      const data = await promise.json();
+      this.coinList = Object.keys(data.Data);
+    })();
   },
 
   mounted() {
@@ -280,6 +399,24 @@ export default {
         }
       },
       deep: true,
+    },
+
+    tickersFilter(newValue) {
+      this.tickersFilter = newValue.toUpperCase().trim();
+      this.tickersPage = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.tickersFilter}&page=${this.tickersPage}`
+      );
+    },
+
+    tickersPage() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.tickersFilter}&page=${this.tickersPage}`
+      );
     },
 
     searchTickerInput(newValue) {
@@ -310,12 +447,23 @@ export default {
     tickers(newValue) {
       this.searchTickerInputIsAlreadyInUse = newValue.some(
         (t) => t.name === this.searchTickerInput
-      )
-        ? true
-        : false;
+      );
     },
   },
   methods: {
+    filteredTickers() {
+      const start = (this.tickersPage - 1) * 6;
+      const end = this.tickersPage * 6;
+
+      const filteredTickers = this.tickers.filter((t) =>
+        t.name.includes(this.tickersFilter)
+      );
+
+      this.tickersHasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+
     subscribeToFetchData(tickerName) {
       setInterval(async () => {
         const promise = await fetch(
@@ -352,6 +500,8 @@ export default {
           value: "-",
         };
         this.tickers.unshift(ticker);
+        this.tickersFilter = "";
+        this.tickersPage = 1;
 
         localStorage.setItem(
           "cryptonomicon-list",
@@ -367,15 +517,7 @@ export default {
     selectTicker(ticker = null) {
       if (ticker !== null && ticker?.name !== this.selectedTickerName) {
         this.selectedTickerName = ticker.name;
-        if (ticker.value !== "-") {
-          const time = new Date();
-          this.graph = [
-            {
-              time: `${time.toTimeString().slice(0, 5)}?`,
-              value: ticker.value,
-            },
-          ];
-        }
+        this.graph = [];
       } else {
         this.selectedTickerName = null;
         this.graph = [];
@@ -394,13 +536,13 @@ export default {
       );
     },
 
-    normalizeGraph(graph) {
+    normalizedGraph() {
       let prices = [];
-      for (const p of graph) prices.push(p.value);
+      for (const p of this.graph) prices.push(p.value);
       const maxValue = Math.max.apply(null, prices);
       const minValue = Math.min.apply(null, prices);
 
-      return graph.map((price) => {
+      return this.graph.map((price) => {
         if (maxValue !== minValue)
           return {
             height: 5 + ((price.value - minValue) * 95) / (maxValue - minValue),
@@ -436,6 +578,20 @@ export default {
 </script>
 
 <style>
+.pagination button {
+  filter: invert(20%) sepia(11%) saturate(1303%) hue-rotate(179deg)
+    brightness(91%) contrast(93%);
+}
+
+.pagination button:hover {
+  filter: unset;
+}
+
+.pagination button:disabled {
+  filter: invert(88%) sepia(2%) saturate(5139%) hue-rotate(185deg)
+    brightness(83%) contrast(78%);
+}
+
 .slide-enter-active,
 .slide-leave-active {
   transition: transform 250ms ease;
